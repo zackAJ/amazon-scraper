2a04d1b6d9718014439601ae2fb2980b73ff61d1
zackAJ <aktouf98zakaria@gmail.com>
2023-11-09 23:56:26 +0100

    added versel config

--- src/scrape.js
+++ src/scrape.js
@@ -1,23 +1,43 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const URL = "https://www.amazon.com/s?k=";
-const options = {
-  headers: {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36",
-  }
+const PRODUCTION_HEADERS = {
+	headers: {
+		Host: "www.amazon.com",
+		"User-Agent":
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0",
+		Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+		"Accept-Language": "en-US,en;q=0.5",
+		"Accept-Encoding": "gzip, deflate, br",
+		Connection: "keep-alive",
+		"Upgrade-Insecure-Requests": "1",
+		TE: "Trailers",
+	},
 };
-const SELECTORS = {//after inspecting the page
+
+const LOCAL_HEADERS = {
+	headers: {
+		"User-Agent":
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36",
+	},
+};
+
+const SELECTORS = {
+	//after inspecting the page
 	products: "[data-component-type=s-search-result]",
-	title: "a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal span.a-color-base.a-text-normal",//this was the hardest to get, the classes are dynamic
+	title:
+		"a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal span.a-color-base.a-text-normal", //this was the hardest to get, the classes are dynamic
 	rating: "span.a-icon-alt",
 	reviews: "span.a-size-base.s-underline-text",
 	image: "img.s-image",
 };
 
+const HEADERS =
+	process.env.APP_ENV == "prod" ? PRODUCTION_HEADERS : LOCAL_HEADERS;
 
 async function fetchAmazonPage(keyword) {
 	try {
-		let res = await axios.get(URL + keyword, options);
+		let res = await axios.get(URL + keyword, HEADERS);
 		return res; //return false to test amazon error response
 	} catch (err) {
 		console.log(err);
