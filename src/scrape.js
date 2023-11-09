e43bd2b8de6efb4e84ad9c4ce7ef1ceaed030a7a
Zakaria AKTOUF <101515566+zackAJ@users.noreply.github.com>
2023-11-09 20:49:52 +0100

    new Headers for production

--- src/scrape.js
+++ src/scrape.js
@@ -3,8 +3,15 @@ const cheerio = require("cheerio");
 const URL = "https://www.amazon.com/s?k=";
 const options = {
 	headers: {
+		Host: "www.amazon.com",
 		"User-Agent":
-			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0",
+		Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+		"Accept-Language": "en-US,en;q=0.5",
+		"Accept-Encoding": "gzip, deflate, br",
+		Connection: "keep-alive",
+		"Upgrade-Insecure-Requests": "1",
+		TE: "Trailers",
 	},
 };
 const SELECTORS = {//after inspecting the page
