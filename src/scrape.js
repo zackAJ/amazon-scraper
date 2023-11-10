3a556ba95f1e9f56d644dfc6fbef80450f3e1252
zackAJ <aktouf98zakaria@gmail.com>
2023-11-10 13:07:49 +0100

    new headers

--- src/scrape.js
+++ src/scrape.js
@@ -11,8 +11,8 @@ const PRODUCTION_HEADERS = {
 		"Accept-Language": "en-US,en;q=0.5",
 		"Accept-Encoding": "gzip, deflate, br",
 		Connection: "keep-alive",
-		"Upgrade-Insecure-Requests": "1",
-		TE: "Trailers",
+		// "Upgrade-Insecure-Requests": "1",
+		// TE: "Trailers",
 	},
 };
 
