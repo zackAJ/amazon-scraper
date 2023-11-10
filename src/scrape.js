3502845a1a2249e94437bd6e126283627d39d149
zackAJ <aktouf98zakaria@gmail.com>
2023-11-10 13:45:24 +0100

    new headers

--- src/scrape.js
+++ src/scrape.js
@@ -5,8 +5,7 @@ const QUERY_URL = "https://www.amazon.com/s?k=";
 const PRODUCTION_HEADERS = {
 	headers: {
 		// Host: "www.amazon.com",
-		"User-Agent":
-			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0",
+		"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36",
 		Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 		"Accept-Language": "en-US,en;q=0.5",
 		"Accept-Encoding": "gzip, deflate, br",
